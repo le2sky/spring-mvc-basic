575820bbb0af981a7cc38c6970a622719bcc50df
le2sky <leehaneul990623@gmail.com>
2023-09-22 23:46:24 +0900

    feat : HTTP 요청 파라미터 - @RequestParam v2

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -5,7 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
@@ -19,5 +22,19 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    /**
+     * @RequestParam 사용 - 파라미터 이름으로 바인딩
+     * @ResponseBody 추가 - View 조회를 무시하고, HTTP message body에 직접 해당 내용 입력
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge
+    ) {
+        log.info("memberName={}, memberAge={}", memberName, memberAge);
+        return "ok";
     }
 }
